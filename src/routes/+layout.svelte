badafbc19eef172f24b402493dc3e80f60f461ff
thaddeuscleo <thaddeuscleo@gmail.com>
2024-03-11 10:43:45 +0000

    feat: add size limit for mobile first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import "../app.css";
 </script>
-
-<div class="w-full min-h-screen p-5 bg-gray-950">  
-  <slot />
+<div class="flex justify-center items-center bg-gray-900">
+  <div class="max-w-screen-sm w-full min-h-screen p-5 bg-gray-950">  
+    <slot />
+  </div>
 </div>
